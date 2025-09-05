1bdee50a4498f3fb7b6a61a259e11114130d163b
daro <mainworkai@gmail.com>
2025-09-05 15:16:55 +0200

    first

---
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.test1
+
+import android.os.Bundle
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    val spinner : Spinner = findViewById<Spinner>(R.id.MySpinner)
+
+    val items = listOf("hobbit", "człowiek", "elf", "krasnolud", "czarodziej")
+
+}
